e29d35f75a3eac62467ce35d22d91fcfde52e35b
marcussassi <marcussassicesar@gmail.com>
2023-03-20 18:51:19 -0300

    First commit

---
+++ primeiroprogramajavaeclipse/src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,22 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+	public static void main(String[] args) {
+		Gato gato = new Gato();
+		Livros livro = new Livros();
+		System.out.println(gato);
+		/*
+		 * Integer a = 5; Integer b = 6; System.out.println("Ola Mundo " + (a+b));
+		 */
+		
+		
+	}
+
+}
+class Livros{
+	public String nome;
+	public String npag;
+	
+}
